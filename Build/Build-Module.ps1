36e1f9021f1b3f08206a2992f7f72262d00e65fa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-24 19:20:58 +0100

    Module builder

---
+++ Build/Build-Module.ps1
@@ -0,0 +1,107 @@
+﻿Build-Module -ModuleName 'DCToolbox' {
+    # Usual defaults as per standard module
+    $Manifest = [ordered] @{
+        ModuleVersion        = '2.0.X'
+        CompatiblePSEditions = @('Desktop', 'Core')
+        GUID                 = '306a16db-02a0-43da-a70c-9e43f9433450'
+        # Author of this module
+        Author               = 'Daniel Chronlund'
+        # Company or vendor of this module
+        CompanyName          = 'Daniel Chronlund'
+        # Copyright statement for this module
+        Copyright            = '(c) 2023 Daniel Chronlund. All rights reserved.'
+        # Description of the functionality provided by this module
+        Description          = 'A PowerShell toolbox for Microsoft 365 security fans.'
+        PowerShellVersion    = '5.1'
+        Tags                 = @("Security", "EntraID")
+        ProjectUri           = 'https://github.com/DanielChronlund/DCToolbox'
+    }
+    New-ConfigurationManifest @Manifest
+
+    # Add standard module dependencies (directly, but can be used with loop as well)
+    New-ConfigurationModule -Type RequiredModule -Name @(
+        'Microsoft.Graph.Applications'
+        'Microsoft.Graph.Authentication'
+        'Microsoft.Graph.Groups'
+        'Microsoft.Graph.Identity.DirectoryManagement'
+        'Microsoft.Graph.Users'
+        'Microsoft.Graph.Identity.SignIns'
+        'Microsoft.Graph.Identity.Governance'
+        'ImportExcel'
+        'MSAL.PS'
+        'AzureAD'
+    ) -Guid 'Auto' -Version 'Latest'
+
+    # Add external module dependencies, using loop for simplicity
+
+    New-ConfigurationModule -Type ExternalModule -Name @(
+        'Microsoft.PowerShell.Security'
+        'Microsoft.PowerShell.Archive'
+        'Microsoft.PowerShell.Utility'
+        'Microsoft.PowerShell.Management'
+    )
+
+    # Add approved modules, that can be used as a dependency, but only when specific function from those modules is used
+    # And on that time only that function and dependant functions will be copied over
+    # Keep in mind it has it's limits when "copying" functions such as it should not depend on DLLs or other external files
+    #New-ConfigurationModule -Type ApprovedModule -Name 'PSSharedGoods', 'PSWriteColor', 'Connectimo', 'PSUnifi', 'PSWebToolbox', 'PSMyPassword'
+
+    New-ConfigurationModuleSkip -IgnoreModuleName @(
+        'powershellget'
+        'PackageManagement'
+    ) -IgnoreFunctionName 'Connect-DCMsGraphAsDelegated'
+
+    $ConfigurationFormat = [ordered] @{
+        RemoveComments                              = $false
+
+        PlaceOpenBraceEnable                        = $true
+        PlaceOpenBraceOnSameLine                    = $true
+        PlaceOpenBraceNewLineAfter                  = $true
+        PlaceOpenBraceIgnoreOneLineBlock            = $false
+
+        PlaceCloseBraceEnable                       = $true
+        PlaceCloseBraceNewLineAfter                 = $true
+        PlaceCloseBraceIgnoreOneLineBlock           = $false
+        PlaceCloseBraceNoEmptyLineBefore            = $true
+
+        UseConsistentIndentationEnable              = $true
+        UseConsistentIndentationKind                = 'space'
+        UseConsistentIndentationPipelineIndentation = 'IncreaseIndentationAfterEveryPipeline'
+        UseConsistentIndentationIndentationSize     = 4
+
+        UseConsistentWhitespaceEnable               = $true
+        UseConsistentWhitespaceCheckInnerBrace      = $true
+        UseConsistentWhitespaceCheckOpenBrace       = $true
+        UseConsistentWhitespaceCheckOpenParen       = $true
+        UseConsistentWhitespaceCheckOperator        = $true
+        UseConsistentWhitespaceCheckPipe            = $true
+        UseConsistentWhitespaceCheckSeparator       = $true
+
+        AlignAssignmentStatementEnable              = $true
+        AlignAssignmentStatementCheckHashtable      = $true
+
+        UseCorrectCasingEnable                      = $true
+    }
+    # format PSD1 and PSM1 files when merging into a single file
+    # enable formatting is not required as Configuration is provided
+    New-ConfigurationFormat -ApplyTo 'OnMergePSM1', 'OnMergePSD1' -Sort None @ConfigurationFormat
+    # format PSD1 and PSM1 files within the module
+    # enable formatting is required to make sure that formatting is applied (with default settings)
+    New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'DefaultPSM1' -EnableFormatting -Sort None
+    # when creating PSD1 use special style without comments and with only required parameters
+    New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'OnMergePSD1' -PSD1Style 'Minimal'
+
+    # configuration for documentation, at the same time it enables documentation processing
+    New-ConfigurationDocumentation -Enable:$false -StartClean -UpdateWhenNew -PathReadme 'Docs\Readme.md' -Path 'Docs'
+
+    New-ConfigurationImportModule -ImportSelf
+
+    New-ConfigurationBuild -Enable:$true -SignModule:$false -DeleteTargetModuleBeforeBuild -MergeModuleOnBuild -MergeFunctionsFromApprovedModules -DoNotAttemptToFixRelativePaths
+
+    New-ConfigurationArtefact -Type Unpacked -Enable -Path "$PSScriptRoot\..\Artefacts\Unpacked" -AddRequiredModules
+    New-ConfigurationArtefact -Type Packed -Enable -Path "$PSScriptRoot\..\Artefacts\Packed" -ArtefactName '<ModuleName>.v<ModuleVersion>.zip'
+
+    # global options for publishing to github/psgallery
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$false
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'CompanyName' -Enabled:$false
+}
